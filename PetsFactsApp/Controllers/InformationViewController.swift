d754da81084f9644789474fb7ca1992ad7ff0d5f
MixonUa <mykhailofrolov@gmail.com>
2023-06-25 21:02:20 +0300

    first commit

---
+++ PetsFactsApp/Controllers/InformationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  InformationViewController.swift
+//  PetsFactsApp
+//
+//  Created by Михаил Фролов on 25.06.2023.
+//
+
+import UIKit
+
+class InformationViewController: UIViewController {
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ PetsFactsApp/Controllers/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  PetsFactsApp
+//
+//  Created by Михаил Фролов on 25.06.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func catButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func dogButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
